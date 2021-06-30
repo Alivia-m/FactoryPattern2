3a866d8dca071b94df44a5a904682fb6e2ec84db
Alivia-m <aliviagayle@icloud.com>
2021-06-30 13:38:59 -0500

    completed to-do list

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace FactoryPattern2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the database you would like to access?");
+            Console.WriteLine("Type: sql");
+            Console.WriteLine("Type: list");
+            Console.WriteLine("Type: mongo");
+
+            var input = Console.ReadLine();
+
+            var db =DataAccessFactory.GetDataAccessType(input);
+
+            var products = db.LoadData();
+            db.SaveData();
+
+            foreach (var product in products)
+            {
+                Console.WriteLine($"{product.Name}, {product.Price}");
+            }
+        }
+    }
+}
---
+++ SQLDataAccess.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryPattern2
+{
+    public class SQLDataAccess : IDataAccess
+    {
+        public static List<Product> Products = new List<Product>()
+        {
+            new Product (){Name = "Laptop", Price = 799},
+            new Product (){Name = "Coffee Maker", Price = 50},
+            new Product (){Name = "Blender", Price = 40},
+            new Product (){Name = "Headphones", Price = 399},
+        };
+        public List<Product> LoadData()
+        {
+            Console.WriteLine("I am reading data from SQL Data Access");
+            return Products;
+        }
+
+        public void SaveData()
+        {
+            Console.WriteLine("I am saving data from a SQL Data Access.");
+        }
+    }
+}
